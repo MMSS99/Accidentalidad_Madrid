3690108e9c2969bfd1e8e272ad04af346ce8842f
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-09-25 16:33:36 +0200

    fix(storage): add storage model and getAccidentList

--- src/main/java/edu/teis/storage/AccidentStorageImpl.java
+++ src/main/java/edu/teis/storage/AccidentStorageImpl.java
@@ -1,4 +1,20 @@
 package edu.teis.storage;
 
+import edu.teis.fetcher.AccidentFetcher;
+import edu.teis.fetcher.AccidentFetcherCsv;
+import edu.teis.model.Accident;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class AccidentStorageImpl {
+
+    private AccidentFetcher fetcher = new AccidentFetcherCsv();
+    private List<Accident> accidentList;
+
+    public List<Accident> getAccidentList(File file) throws IOException {
+        accidentList = accidentList.isEmpty() ? fetcher.readFromFile(file) : accidentList;
+        return accidentList;
+    }
 }
