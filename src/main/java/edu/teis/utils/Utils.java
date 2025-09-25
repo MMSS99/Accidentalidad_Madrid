c9371fa23252d18cb2037005309c0603aae20374
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-09-25 16:04:07 +0200

    feat(utils):  add stringtotime to parse the string in csv to LocalTime

--- src/main/java/edu/teis/utils/Utils.java
+++ src/main/java/edu/teis/utils/Utils.java
@@ -3,6 +3,7 @@ package edu.teis.utils;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Utils {
@@ -12,7 +13,7 @@ public class Utils {
     }
 
     public static LocalTime stringToTime(String time) throws ParseException {
-        // LocalTime.parse() ?????
-        return ;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return LocalTime.parse(time, formatter);
     }
 }
