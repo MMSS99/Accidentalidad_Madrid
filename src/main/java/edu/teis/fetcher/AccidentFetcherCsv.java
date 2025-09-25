1c672e50007375d9b96741967ec59bbf31958dfa
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-09-25 16:24:30 +0200

    feat(fetcher):  finish readFromFile

--- src/main/java/edu/teis/fetcher/AccidentFetcherCsv.java
+++ src/main/java/edu/teis/fetcher/AccidentFetcherCsv.java
@@ -6,10 +6,11 @@ import edu.teis.model.Accident;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.text.ParseException;
 import java.util.List;
 
 public class AccidentFetcherCsv implements AccidentFetcher {
-    public List<Accident> readFromFile(File file) throws IOException {
+    public List<Accident> readFromFile(File file) throws IOException{
         if (!file.isFile() || !file.canRead()) {
             throw new IllegalArgumentException("The given CSV file is not a file or is nto readable.");
         }
@@ -17,15 +18,35 @@ public class AccidentFetcherCsv implements AccidentFetcher {
         return Files.lines(file.toPath())
                 .skip(1)
                 .map(line -> line.replace("; ", " - ").split(";")) //replace casos de ; .
-                .map(this::createAccident)
+                .map(rawData -> { try {return createAccident(rawData); } catch (ParseException e) {e.printStackTrace(); return null; }})
                 .toList();
 
 
 
     }
 
-    private Accident createAccident (String[] accidentRawData){
-        return new Accident();
+    private Accident createAccident (String[] accidentRawData) throws ParseException {
+        return new Accident(
+                accidentRawData[0],
+                accidentRawData[1],
+                accidentRawData[2],
+                accidentRawData[3],
+                accidentRawData[4],
+                accidentRawData[5],
+                accidentRawData[6],
+                accidentRawData[7],
+                accidentRawData[8],
+                accidentRawData[9],
+                accidentRawData[10],
+                accidentRawData[11],
+                accidentRawData[12],
+                accidentRawData[13],
+                accidentRawData[14],
+                accidentRawData[15],
+                accidentRawData[16],
+                accidentRawData[17],
+                accidentRawData[18]
+        );
     }
 
 
