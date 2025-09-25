532ab173ed71aab9653e0c1c6b8dc7fd1c6ee930
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-09-25 16:26:35 +0200

    fix(fetcher): add IOException throwable to interface

--- src/main/java/edu/teis/fetcher/AccidentFetcher.java
+++ src/main/java/edu/teis/fetcher/AccidentFetcher.java
@@ -3,8 +3,9 @@ package edu.teis.fetcher;
 import edu.teis.model.Accident;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public interface AccidentFetcher {
-    List<Accident> readFromFile(File file);
+    List<Accident> readFromFile(File file) throws IOException;
 }
