b2580e8de1f1923967832926e78436d08b1e26b5
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-09-25 16:16:42 +0200

    feat(model):  add Accident builder

--- src/main/java/edu/teis/model/Accident.java
+++ src/main/java/edu/teis/model/Accident.java
@@ -1,9 +1,74 @@
 package edu.teis.model;
 
+import edu.teis.utils.Utils;
+
+import java.text.ParseException;
+import java.time.LocalTime;
 import java.util.Date;
+import java.util.Optional;
 
 public class Accident {
-    private final String codigo_expediente;
-    private final Date fecha;
+    private final String num_expedient;
+    private final Date date;
+    private final LocalTime time;
+    private final String localization;
+    private final String case_number;
+    private final Integer district_code;
+    private final String district_name;
+    private final String accident_type;
+    private final Optional<String> weather;
+    private final String vehicle_type;
+    private final String person_type;
+    private final String person_agerange;
+    private final String person_gender;
+    private final Optional<Integer> harm_code;
+    private final Optional<String> harm;
+    private final Long coords_x;
+    private final Long coords_y;
+    private final Boolean alcohol;
+    private final Boolean drugs;
+
+    public Accident(String num_expedient,
+                    String date,
+                    String time,
+                    String localization,
+                    String case_number,
+                    String district_code,
+                    String district_name,
+                    String accident_type,
+                    String weather,
+                    String vehicle_type,
+                    String person_type,
+                    String person_agerange,
+                    String person_gender,
+                    String harm_code,
+                    String harm,
+                    String coords_x,
+                    String coords_Y,
+                    String alcohol,
+                    String drugs) throws ParseException {
+        this.num_expedient = num_expedient;
+        this.date = Utils.stringToDate(date);
+        this.time = Utils.stringToTime(time);
+        this.localization = localization;
+        this.case_number = case_number;
+        this.district_code = Integer.parseInt(district_code);
+        this.district_name = district_name;
+        this.accident_type = accident_type;
+        this.weather = weather.isEmpty() ? Optional.empty() : Optional.of(weather);
+        this.vehicle_type = vehicle_type;
+        this.person_type = person_type;
+        this.person_agerange = person_agerange;
+        this.person_gender = person_gender;
+        this.harm_code = harm_code.isEmpty() ? Optional.empty() : Optional.of(Integer.parseInt(harm));
+        this.harm = harm.isEmpty() ? Optional.empty() : Optional.of(harm);
+        this.coords_x = Long.parseLong(coords_x);
+        this.coords_y = Long.parseLong(coords_Y);
+        this.alcohol = !alcohol.isEmpty() && !alcohol.equals("N");
+        this.drugs = !drugs.isEmpty();
+    }
+
+
+
 
 }
