2da7ce361bd22896f058d426deb3e4341820d3c6
MMSS99 <mmssalgado09@gmail.com>
2025-09-25 12:08:00 +0200

    feat(AccidentFetcher): setup main reading process

--- src/main/java/edu/teis/fetcher/AccidentFetcherCsv.java
+++ src/main/java/edu/teis/fetcher/AccidentFetcherCsv.java
@@ -1,4 +1,28 @@
 package edu.teis.fetcher;
 
-public class AccidentFetcherCsv {
+
+import edu.teis.model.Accident;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class AccidentFetcherCsv implements AccidentFetcher {
+    public List<Accident> readFromFile(File file) throws IOException {
+        if (!file.isFile() || !file.canRead()) {
+            throw new IllegalArgumentException("The given CSV file is not a file or is nto readable.");
+        }
+
+        return Files.lines(file.toPath())
+                .skip(1)
+                .map() //replace casos de ; .
+
+
+
+    }
+
+    private Accident createAccident (
+
+    )
 }
