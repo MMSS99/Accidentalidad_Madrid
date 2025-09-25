344536e5f625506566c66c09e227aa2fb06397d5
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-09-25 16:55:09 +0200

    fix(storage): initilize accidentList variable

--- src/main/java/edu/teis/storage/AccidentStorageImpl.java
+++ src/main/java/edu/teis/storage/AccidentStorageImpl.java
@@ -6,12 +6,13 @@ import edu.teis.model.Accident;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AccidentStorageImpl implements AccidentStorage {
 
     private AccidentFetcher fetcher = new AccidentFetcherCsv();
-    private List<Accident> accidentList;
+    private List<Accident> accidentList = new ArrayList<Accident>();
 
     @Override
     public void loadAccidents(File file) throws IOException {
